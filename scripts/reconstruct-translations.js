79558f4ce0ba724cdb2d00351f9b46a5e6f4aef5
Ilya Gushchin <frylock3434@gmail.com>
2021-02-26 16:41:46 +0300

    fix reconstruction script

--- scripts/reconstruct-translations.js
+++ scripts/reconstruct-translations.js
@@ -19,36 +19,35 @@ var fs = require('fs')
 
 var Skeletonfilelist = []
 Skeletonfilelist = walkSync("skeletons/", Skeletonfilelist).filter(filename => filename.endsWith("skl.md"))
-var XliffFileList = []
-XliffFileList = walkSync("xliff/", XliffFileList).filter(
-    (filename, index) => {
-        let skeletonFilename = Skeletonfilelist[index]
-        return filename.endsWith(".xlf")
-    }
-)
-if (fs.existsSync("reconstructed")) {
-    fs.rmdirSync("reconstructed", { recursive: true })
-}
-fs.mkdirSync("reconstructed")
 
-if (Skeletonfilelist.length == XliffFileList.length) {
+process.argv.slice(2).forEach((targetLang) => {
+    var XliffFileList = []
+    XliffFileList = walkSync(`${targetLang}_xliff/`, XliffFileList).filter(
+        (filename, index) => {
+            let skeletonFilename = Skeletonfilelist[index]
+            return filename.endsWith(".xlf")
+        }
+    )
+    if (fs.existsSync(`${targetLang}_reconstructed`)) {
+        fs.rmdirSync(`${targetLang}_reconstructed`, { recursive: true })
+    }
+    fs.mkdirSync(`${targetLang}_reconstructed`)
+
     Skeletonfilelist.forEach((skeletonFile, index) => {
         let xliffFile = XliffFileList[index]
         let skeleton = fs.readFileSync(skeletonFile, "utf8")
         let xliff = fs.readFileSync(xliffFile, "utf8")
         let nameItems = skeletonFile.substring(10).split("_")
-        let targetLang = nameItems[0].substring(5, 7)
-        if (!fs.existsSync(`reconstructed/${targetLang}`)) {
-            fs.mkdirSync(`reconstructed/${targetLang}`)
-        }
         let folder = nameItems[1]
-        if (!fs.existsSync(`reconstructed/${targetLang}/${folder}`)) {
-            fs.mkdirSync(`reconstructed/${targetLang}/${folder}`)
+        if (!fs.existsSync(`${targetLang}_reconstructed/${folder}`)) {
+            fs.mkdirSync(`${targetLang}_reconstructed/${folder}`)
         }
         let name = nameItems[2].split(".skl.").join(".")
         let result = reconstruct(xliff, skeleton, function (err, translatedMd) {
             if (err) throw new Error(err);
-            fs.writeFileSync(`reconstructed/${targetLang}/${folder}/` + name, translatedMd)
+            fs.writeFileSync(`${targetLang}_reconstructed/${folder}/` + name, translatedMd)
         })
     });
-}
+})
+
+
